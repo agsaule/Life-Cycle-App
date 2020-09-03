9dcbab55fa57e70a501bfad90891e885b056a9e0
agsaule <agsaule@hotmail.com>
2020-09-03 23:07:26 +0300

    Initial Commit

---
+++ Life Cycle App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life Cycle App
+//
+//  Created by agnese.saulite on 03/09/2020.
+//  Copyright © 2020 AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
